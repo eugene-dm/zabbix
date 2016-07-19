c5a9db63d013accff63a7a1d16ac8bfae5bfe9d8
Eugene Dmytrychenko <dm@voicebase.com>
2016-07-19 14:31:08 +0300

    changes for proxy servers list

--- attributes/proxy.rb
+++ attributes/proxy.rb
@@ -3,3 +3,4 @@ include_attribute 'zabbix'
 default['zabbix']['proxy']['install_method'] = 'source'
 default['zabbix']['proxy']['hostname'] = node['fqdn']
 default['zabbix']['proxy']['config']['ConfigFrequency'] = 60
+default['zabbix']['proxy']['servers'] = []
