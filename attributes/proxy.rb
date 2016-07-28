36d0d317221f7672ad927107ee9b65167d3cef8c
Eugene Dmytrychenko <dm@voicebase.com>
2016-07-28 15:59:46 +0300

    returned lost java params

--- attributes/proxy.rb
+++ attributes/proxy.rb
@@ -7,3 +7,4 @@ default['zabbix']['proxy']['start_trappers'] = 5
 default['zabbix']['proxy']['start_pingers'] = 1
 default['zabbix']['proxy']['timeout'] = '3'
 default['zabbix']['proxy']['cache_size'] = '8M' # default 8MB
+default['zabbix']['proxy']['servers'] = []
