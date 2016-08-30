023ae6b28f40b80df58ccc8e06a29217e7707640
Eugene Dmytrychenko <dm@voicebase.com>
2016-08-30 16:55:36 +0300

    added nging ssl support

--- attributes/web.rb
+++ attributes/web.rb
@@ -4,6 +4,9 @@ default['zabbix']['web']['install_method']  = 'apache'
 default['zabbix']['web']['fqdn']            = node['fqdn']
 default['zabbix']['web']['aliases']         = ['zabbix']
 default['zabbix']['web']['port']            = 80
+default['zabbix']['web']['cert']            = nil
+default['zabbix']['web']['key']             = nil
+default['zabbix']['web']['ssl']             = false
 
 default['zabbix']['web']['php']['fastcgi_listen'] = '127.0.0.1:9000' # only applicable when using php-fpm (nginx)
 default['zabbix']['web']['php']['settings']    = {
